6622f66a605f39567f32f3c031e036183938d6a4
Vadim <vadim@plexeo.net>
2025-09-11 17:18:06 +0500

    mixed nested types

--- src/ListTabulator/index.ts
+++ src/ListTabulator/index.ts
@@ -365,18 +365,15 @@ export default class ListTabulator<Renderer extends ListRenderer> {
   public pasteHandler(element: PasteEvent['detail']['data']): ListData {
     const { tagName: tag } = element;
     let style: ListDataStyle = 'unordered';
-    let tagToSearch: string;
 
     // set list style and tag to search.
     switch (tag) {
       case 'OL':
         style = 'ordered';
-        tagToSearch = 'ol';
         break;
       case 'UL':
       case 'LI':
         style = 'unordered';
-        tagToSearch = 'ul';
     }
 
     const data: ListData = {
@@ -400,7 +397,7 @@ export default class ListTabulator<Renderer extends ListRenderer> {
 
       return children.map((child) => {
         // get subitems if they exist.
-        const subItemsWrapper = child.querySelector(`:scope > ${tagToSearch}`);
+        const subItemsWrapper = child.querySelector(':scope > ol, :scope > ul');
         // get subitems.
         const subItems = subItemsWrapper ? getPastedItems(subItemsWrapper) : [];
         // get text content of the li element.
