7b08812424aeac0610cb9d757efd6fb4d0ac7260
Vadim <vadim@plexeo.net>
2025-09-12 12:56:02 +0500

    trim text content

--- src/ListTabulator/index.ts
+++ src/ListTabulator/index.ts
@@ -401,7 +401,7 @@ export default class ListTabulator<Renderer extends ListRenderer> {
         // get subitems.
         const subItems = subItemsWrapper ? getPastedItems(subItemsWrapper) : [];
         // get text content of the li element.
-        const content = child.firstChild?.textContent ?? '';
+        const content = child.firstChild?.textContent?.trim() ?? '';
 
         return {
           content,
